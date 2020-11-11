7e89ef7fd5f6ce8e7ea3e44347acf503224153e0
Aza Maulana <azamaulanaaa@gmail.com>
2020-11-12 01:41:06 +0700

    add http router with gorilla/mux

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"net/http"
 	"ohkaca/lib/storage"
 
 	"github.com/anacrolix/torrent"
+	"github.com/gorilla/mux"
 )
+
+const host = "localhost:3000"
 
 func main() {
 	customConfig := torrent.NewDefaultClientConfig()
@@ -12,6 +16,9 @@ func main() {
 	c, err := torrent.NewClient(customConfig)
 	must(err)
 	defer c.Close()
+
+	router := mux.NewRouter()
+	http.ListenAndServe(host, router)
 }
 
 func must(err error) {
