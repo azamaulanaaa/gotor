f2a4a9dfad2ba10ad96a50b871b6715a8c87d47b
Aza Maulana <azamaulanaaa@gmail.com>
2022-01-19 10:50:50 +0700

    build: nix dev environment

---
+++ shell.nix
@@ -0,0 +1,10 @@
+{pkgs ? import <nixpkgs> {}} :
+pkgs.mkShell {
+    packages = with pkgs; [
+        go_1_17
+        gopls
+    ];
+    shellHook = ''
+        export PATH=$PATH:$(go env GOPATH)/bin
+    '';
+}
