fdbf31ea4181a344ab94cb9f3e92ada780ccca30
Aza Maulana <azamaulanaaa@gmail.com>
2022-01-22 08:01:03 +0700

    feat: add implementable torrent http handler
    it is kind a refactor with reducing use of external module. torrent http
    file serve has its own structure. this mean easier to implement as go
    external libraries instead fork it.

--- src/http/http.go
+++ src/http/http.go
@@ -3,61 +3,27 @@ package http
 import (
     "fmt"
     "net/http"
-    "path/filepath"
-    "time"
 
     "github.com/azamaulanaaa/gotor/src/torrentlib"
-
-    gorilla "github.com/gorilla/mux"
 )
 
 type HttpServer struct {
     torrentClient   *torrentlib.Client
     port            uint16
-    router          *gorilla.Router
+    httpHandler     http.Handler
 }
 
 func NewHttpServer(port uint16, torrentClient *torrentlib.Client) HttpServer {
     httpServer := HttpServer {
         torrentClient:  torrentClient,
         port:           port,
-        router:         gorilla.NewRouter(),
+        httpHandler:    NewTorrentHttpHanndler(torrentClient),
     }
-
-    httpServer.router.PathPrefix("/{hash:[\\w\\d]+}/").HandlerFunc(httpServer.torrentServe)
 
     return httpServer
 }
 
 func (httpServer HttpServer) ListenAndServe() error {
     host := fmt.Sprintf(":%d", httpServer.port)
-    return http.ListenAndServe(host, httpServer.router)
-}
-
-func (httpServer HttpServer) torrentServe(rw http.ResponseWriter, r *http.Request) {
-    hash := gorilla.Vars(r)["hash"]
-    path, _ := filepath.Rel("/" + hash, r.URL.Path)
-    
-    torrent, err := httpServer.torrentClient.AddHash(hash)
-    if err != nil {
-        rw.WriteHeader(http.StatusNotFound)
-        return 
-    }
-
-    files := torrent.Files()
-    for _, file := range files {
-        if file.Path() != path {
-            continue
-        }
-        
-        reader := file.Reader()
-        defer reader.Close()
-        basename := filepath.Base(file.Path())
-        http.ServeContent(rw, r, basename, time.Unix(0,0), reader)
-
-        return
-    }
-
-    rw.WriteHeader(http.StatusNotFound)
-    return
+    return http.ListenAndServe(host, httpServer.httpHandler)
 }
---
+++ src/http/torrent_serve.go
@@ -0,0 +1,58 @@
+package http
+
+import (
+	"io"
+	"net/http"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/azamaulanaaa/gotor/src/torrentlib"
+)
+
+type TorrentHttpHandler struct{
+    torrentClient *torrentlib.Client
+}
+
+func NewTorrentHttpHanndler(client *torrentlib.Client) TorrentHttpHandler {
+    return TorrentHttpHandler{
+        torrentClient: client,
+    }
+}
+
+func (torrentServe TorrentHttpHandler) Reader(hash string, path string) (io.ReadSeekCloser, error) {
+    torrent, err := torrentServe.torrentClient.AddHash(hash)
+    if err != nil {
+        return nil, io.ErrUnexpectedEOF
+    }
+
+    files := torrent.Files()
+    for _, file := range files {
+        if file.Path() != path {
+            continue
+        }
+        
+        reader := file.Reader()
+        return reader, nil
+    }
+    
+    return nil, io.ErrUnexpectedEOF
+}
+
+/*
+    Handle torrent file requiest with url path /[torrent hash]/[file path]
+*/
+func (torrentServe TorrentHttpHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+    urlPath := r.URL.Path
+    hash := strings.Split(urlPath,"/")[1]
+    path, _ := filepath.Rel("/" + hash, r.URL.Path)
+    basename := filepath.Base(path)
+    reader, err := torrentServe.Reader(hash, path)
+    if err != nil {
+        rw.WriteHeader(http.StatusNotFound)
+    }
+    defer reader.Close()
+
+    http.ServeContent(rw, r, basename, time.Unix(0,0), reader)
+    return
+}
