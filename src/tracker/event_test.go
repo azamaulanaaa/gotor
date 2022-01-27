feeebdc63256aa4ae92e89f8bddb0912a73e122d
Aza Maulana <azamaulanaaa@gmail.com>
2022-01-28 00:05:12 +0700

    test: add event to string

--- src/tracker/event_test.go
+++ src/tracker/event_test.go
@@ -20,7 +20,7 @@ func TestNewEventFromString(t *testing.T) {
         output  Output
     }
 
-    testCases :=[]TestCase{
+    testCases := []TestCase{
         { Input{ "started", }, Output{ EventStarted, nil, }, },
         { Input{ "stopped", }, Output{ EventStopped, nil, }, },
         { Input{ "completed", }, Output{ EventCompleted, nil, }, },
@@ -31,6 +31,36 @@ func TestNewEventFromString(t *testing.T) {
     for _, testCase := range testCases {
         var output Output
         output.event, output.err = NewEvent(testCase.input.value)
+        if reflect.DeepEqual(testCase.output, output) == false {
+            t.Errorf("\ngive\t: %v\nexpect\t: %v\ngot\t: %v",testCase.input, testCase.output, output)
+        }
+    }
+}
+
+func TestEventToString(t *testing.T) {
+    type Input struct {
+        event   Event
+    }
+
+    type Output struct {
+        value   string
+    }
+
+    type TestCase struct {
+        input   Input
+        output  Output
+    }
+
+    testCases := []TestCase{
+        { Input{ EventStarted }, Output { "started" }, },
+        { Input{ EventStopped }, Output { "stopped" }, },
+        { Input{ EventCompleted }, Output { "completed" }, },
+    }
+
+    for _, testCase := range testCases {
+        var output Output
+        output.value = testCase.input.event.String()
+
         if reflect.DeepEqual(testCase.output, output) == false {
             t.Errorf("\ngive\t: %v\nexpect\t: %v\ngot\t: %v",testCase.input, testCase.output, output)
         }
