a6e1742e35edf776238bbeb6dda3b27269d0cfce
Aza Maulana <azamaulanaaa@gmail.com>
2022-02-15 00:23:35 +0700

    test!: test should be catagorize properly

--- src/tracker/decode_test.go
+++ src/tracker/decode_test.go
@@ -1,3 +1,5 @@
+//go:build unit
+
 package tracker_test
 
 import (
@@ -11,32 +13,32 @@ import (
 )
 
 type requestTestData struct {
-    Request tracker.Request
-    EncodedRequest string
+	Request        tracker.Request
+	EncodedRequest string
 }
 
 func TestDecodeRequest(t *testing.T) {
-    var infohash hash.Hash
-    copy(infohash[:],"ahashshoudlworkswell")
+	var infohash hash.Hash
+	copy(infohash[:], "ahashshoudlworkswell")
 
-    var peerID peer.PeerID
-    copy(peerID[:], "otherhashshouldworks")
+	var peerID peer.PeerID
+	copy(peerID[:], "otherhashshouldworks")
 
-    testsData := requestTestData{
-            tracker.Request{
-                infohash,
-                peerID,
-                32,
-                12,
-                0,
-                tracker.EventCompleted,
-                net.IPv4(123,123,123,123),
-                80,
-            },
-            "info_hash=ahashshoudlworkswell&downloaded=32&ip=123.123.123.123&event=completed&port=80&left=12&completed=0&peer_id=otherhashshouldworks",
-    }
+	testsData := requestTestData{
+		tracker.Request{
+			infohash,
+			peerID,
+			32,
+			12,
+			0,
+			tracker.EventCompleted,
+			net.IPv4(123, 123, 123, 123),
+			80,
+		},
+		"info_hash=ahashshoudlworkswell&downloaded=32&ip=123.123.123.123&event=completed&port=80&left=12&completed=0&peer_id=otherhashshouldworks",
+	}
 
-    out, err := tracker.DecodeRequest(testsData.EncodedRequest)
-    test.Ok(t, err)
-    test.Equals(t, testsData.Request, out)
+	out, err := tracker.DecodeRequest(testsData.EncodedRequest)
+	test.Ok(t, err)
+	test.Equals(t, testsData.Request, out)
 }
