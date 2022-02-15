81d53975f64ef710e8829a181974e1e86c7ce7e9
Aza Maulana <azamaulanaaa@gmail.com>
2022-02-15 10:21:26 +0700

    test: unit test for encoding tracker request

---
+++ src/tracker/encode_test.go
@@ -0,0 +1,44 @@
+//go:build unit
+
+package tracker_test
+
+import (
+	"net"
+	"net/url"
+	"testing"
+
+	"github.com/azamaulanaaa/gotor/src/hash"
+	"github.com/azamaulanaaa/gotor/src/peer"
+	"github.com/azamaulanaaa/gotor/src/tracker"
+	"github.com/azamaulanaaa/gotor/test"
+)
+
+func TestEncodeRequest(t *testing.T) {
+	var infohash hash.Hash
+	copy(infohash[:], "ahashshoudlworkswell")
+
+	var peerID peer.PeerID
+	copy(peerID[:], "otherhashshouldworks")
+
+	var urlQuery url.Values
+	urlQuery, err := url.ParseQuery("info_hash=ahashshoudlworkswell&downloaded=32&ip=123.123.123.123&event=completed&port=80&left=12&uploaded=0&peer_id=otherhashshouldworks")
+	test.Ok(t, err)
+
+	testsData := requestTestData{
+		tracker.Request{
+			infohash,
+			peerID,
+			32,
+			12,
+			0,
+			tracker.EventCompleted,
+			net.IPv4(123, 123, 123, 123),
+			80,
+		},
+        urlQuery,
+	}
+
+    our, err := tracker.EncodeRequest(testsData.request)
+    test.Ok(t, err)
+    test.Equals(t, testsData.UrlQuery.Encode(), our)
+}
